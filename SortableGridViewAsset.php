21e7d8ab83363a7870469b365963642c851eee4c
Juozas Lizdenis <lizdenis.juozas@gmail.com>
2020-05-07 21:48:53 +0300

    fixed asset path

--- SortableGridViewAsset.php
+++ SortableGridViewAsset.php
@@ -5,7 +5,7 @@ namespace juozasl\sortable;
 use yii\web\AssetBundle;
 
 class SortableGridViewAsset extends AssetBundle {
-    public $sourcePath = '@vendor/richardfan1126/yii2-sortable-gridview/assets';
+    public $sourcePath = '@vendor/juozasl/yii2-sortable-gridview/assets';
     public $baseUrl = '@web';
     public $js = [
         'js/jquery.ui.touch-punch.min.js',
